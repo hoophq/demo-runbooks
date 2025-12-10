2d5caeb4577c4a8a549470b61b9b06afe3c4af85
Nathan Aronson <nsaronson@gmail.com>
2025-12-10 14:26:43 -0500

    Update create_secret.runbook.sh

--- aws/create_secret.runbook.sh
+++ aws/create_secret.runbook.sh
@@ -1,3 +1,3 @@
 aws secretsmanager create-secret \
-  --name {{ .secret_name }}
+  --name {{ .secret_name }} \
   --secret-string '{ "{{ .secret_key | required "true" }}" : "{{ .secret_value | required "true"}}" }'
