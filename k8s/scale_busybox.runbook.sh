196c64f594c76ff4a6c4671a064eb99d8ef26deb
Nathan Aronson <nsaronson@gmail.com>
2025-12-10 10:53:55 -0500

    Update scale_busybox.runbook.sh

--- k8s/scale_busybox.runbook.sh
+++ k8s/scale_busybox.runbook.sh
@@ -1,1 +1,1 @@
-kubectl scale deployment {{ .deployment_name | options "busybox" }} --replicas={{ .num_replicas | options "1" "2" "3" }}
+kubectl scale deployment {{ .deployment_name | type "select" | options "busybox" }} --replicas={{ .num_replicas | options "1" "2" "3" }}
